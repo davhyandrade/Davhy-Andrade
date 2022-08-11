fff1a697ac81921c625a5d1545d19710243362c7
Davhy Andrade <davhyhello@gmail.com>
2022-08-11 00:14:03 -0300

    add: corrections

--- _assets/_js/menu-fixo.js
+++ _assets/_js/menu-fixo.js
@@ -2,11 +2,11 @@
 setInterval(() => {
     
     if(window.scrollY >= 800) {
-    menuFixed.classList.add('menu-fixed')
+        menuFixed.classList.add('menu-fixed')
     }
 
     else if(window.scrollY < 800) {
-    menuFixed.classList.remove('menu-fixed')
+        menuFixed.classList.remove('menu-fixed')
     }
 
 }, 100);
